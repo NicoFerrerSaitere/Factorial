d3594a751fe416a979d14b9b59c97af4f19a0095
Nico Ferrer <165787964+NicoFerrerSaitere@users.noreply.github.com>
2024-04-03 16:07:07 +0200

    Add files via upload

---
+++ factorial/src/main/java/vn/eclipse/factorial/App.java
@@ -0,0 +1,9 @@
+package vn.eclipse.factorial;
+
+public class App 
+{
+	public static void main(String[]args) {
+		Calculadora calc = new Calculadora();	
+		calc.CalcFactorial(4);
+}
+}
---
+++ factorial/src/test/java/vn/eclipse/factorial/testFactorial.java
@@ -0,0 +1,15 @@
+package vn.eclipse.factorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testFactorial {
+
+	@Test
+	public void test() {
+	int num = 0;
+	
+	}
+
+}
