825ecee1180ffce261fef4c0b084a37461e98fd5
NicoFerrerSaitere <165787964+NicoFerrerSaitere@users.noreply.github.com>
2024-04-09 13:34:54 +0200

    Add files via upload

---
+++ src/main/java/com/viewnext/exception/CalculadoraFactorialException.java
@@ -0,0 +1,12 @@
+package com.viewnext.exception;
+
+public class CalculadoraFactorialException extends Exception {
+
+	public CalculadoraFactorialException() {
+	}
+
+	public CalculadoraFactorialException(String msj_error) {
+		super(msj_error);
+	}
+
+}
---
+++ src/test/java/com/viewnext/calculadora/tests/Calculadora1Tests.java
@@ -0,0 +1,50 @@
+package com.viewnext.calculadora.tests;
+
+
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import com.viewnext.calculadora.Calculadora;
+import com.viewnext.exception.CalculadoraFactorialException;
+
+public class Calculadora1Tests {
+
+	@Test
+	public void factorial0() throws Exception {
+
+		Calculadora apiCalculadora = new Calculadora();
+		long nfactorial = Calculadora.factorial(0);
+		Assertions.assertEquals(1, nfactorial);
+	}
+
+	@Test
+	public void factorial1() throws Exception {
+
+		Calculadora apiCalculadora = new Calculadora();
+		long nfactorial = Calculadora.factorial(1);
+		Assertions.assertEquals(1, nfactorial);
+	}
+
+	@Test
+	public void factorialmas() throws Exception {
+
+		Calculadora apiCalculadora = new Calculadora();
+		long nfactorial = Calculadora.factorial(5);
+		Assertions.assertEquals(120, nfactorial);
+
+	}
+
+
+	@Test
+	public void factorialmenos() throws Exception {
+		
+		Calculadora apiCalculadora = new Calculadora();
+
+		Exception exception = assertThrows(CalculadoraFactorialException.class, () ->
+		Calculadora.factorial(-1)
+		);
+        
+	}
+}
